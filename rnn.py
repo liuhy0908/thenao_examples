3735d5b822f0ca364a89e618a952e1344053edf9
Shashwat Kumar <thegreatshasha@gmail.com>
2016-03-15 11:35:58 +0530

    add *

--- rnn.py
+++ rnn.py
@@ -1,23 +1,67 @@
 import numpy as np
 import theano
+from theano import tensor as t, printing
+from theano.tests.breakpoint import PdbBreakpoint
+from load_data import x_vec, y_vec, vocab
 
-class RNN:
-    def __init__(self, hidden_size, input_size, output_size):
-        self.W_h = theano.shared(np.random.uniform(size=(hidden_size, hidden_size), low=-.01, high=.01))
-        self.W_x = theano.shared(np.random.uniform(size=(hidden_size, input_size), low=-.01, high=.01))
-        self.W_y = theano.shared(np.random.uniform(size=(output_size, hidden_size), low=-.01, high=.01))
-        self.h = 0
-        self.b = 0
+theano.config.optimizer = 'None'
 
-    def step(self, x):
-        self.h = se
+hidden_size = input_size = output_size = vocab['size'];
 
-    def predict(self, x_vec):
+#class RNN:
+#    def __init__(, hidde   n_size, input_size, output_size):
+#W_h = theano.shared(np.zeros((hidden_size, hidden_size)))
+#W_x = theano.shared(np.ones((hidden_size, input_size)))
+#W_y = theano.shared(np.ones((output_size, hidden_size)))
+W_h = theano.shared(np.random.uniform(size=(hidden_size, hidden_size), low=-.01, high=.01))
+W_x = theano.shared(np.random.uniform(size=(hidden_size, input_size), low=-.01, high=.01))
+W_y = theano.shared(np.random.uniform(size=(output_size, hidden_size), low=-.01, high=.01))
+
+# Define Inputs
+x = t.matrix()
+y = t.matrix()
+
+h0 = t.vector()
+
+lr = t.scalar()
+
+def step(x_t, h_t_1, W_h, W_x, W_y):
+    # Add breakpoint
+
+    h = t.tanh(theano.dot(W_h, h_t_1) + theano.dot(W_x, x_t))
+    y = theano.dot(W_y, h)
+    return h, y
+
+    #def predict(, x_vec):
         # return symbolic output of theano pass
-        pass
+[h, out], _ = theano.scan(step, sequences=x, outputs_info=[h0, None], non_sequences=[W_h, W_x, W_y])
 
-    def train(self, x_vec, y_vec)
+error = ((out - y)**2).sum()
 
+gW_h, gW_x, gW_y = t.grad(error, [W_h, W_x, W_y])
 
-def main()
-    x = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]
+output = theano.function([h0, x, y], [out, error], on_unused_input='warn')
+
+train = theano.function([h0, x, y, lr], [error], updates={W_h: W_h - lr * gW_h, W_x: W_x - lr * gW_x, W_y: W_y - lr * gW_y})
+
+#x_in = np.array([[1,1,1],[2,2,2]])
+#y_in = np.cumsum(x_in, axis=1)
+#print y_in
+#import pdb; pdb.set_trace()
+h0_in = np.zeros(vocab['size'])
+#print output(h0_in, x_in.transpose(), y_in.transpose())
+
+#net = RNN(4,4,4)
+#pred = net.predict(x)
+#main()
+
+# too much error, implement batching
+#
+
+for i in range(5000):
+    idx = np.random.randint(len(x_vec)-3)
+    lr = 0.01
+    #print lr
+    print(train(h0_in, x_vec[idx:idx+3].transpose(), y_vec[idx:idx+3].transpose(), lr))
+
+# Use trained model
